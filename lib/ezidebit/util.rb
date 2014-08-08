f684a612e341f45ecbaea5121030c29eef13bc75
Jason Stafford <jason@softwareallies.com>
2014-08-07 20:20:07 -0500

    added util class

---
+++ lib/ezidebit/util.rb
@@ -0,0 +1,10 @@
+module Ezidebit
+  class Util
+
+    #Nokogiri::XML::Builder uses method missing to build xml elements, 
+    #send will still raise method missing and create elements dynamically
+    def self.generate_xml_options(xml, options={})
+    end
+
+  end
+end
