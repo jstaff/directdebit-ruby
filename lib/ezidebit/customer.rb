a3594b9c50f147aa1f395c100e0053fef1e8349b
Jason Stafford <jason@softwareallies.com>
2014-08-15 12:10:02 -0500

    implemented edit_customer_details and add_bank_debit methods

--- lib/ezidebit/customer.rb
+++ lib/ezidebit/customer.rb
@@ -2,22 +2,35 @@ module Ezidebit
   class Customer < EzidebitObject
 
   	#TODO: Use Base API URL and add action to it
-    SOAP_ACTION='https://px.ezidebit.com.au/INonPCIService/AddCustomer'
+    ADD_CUSTOMER_ACTION='https://px.ezidebit.com.au/INonPCIService/AddCustomer'
+    EDIT_CUSTOMER_ACTION='https://px.ezidebit.com.au/INonPCIService/EditCustomerDetails'
     UPDATE_STATUS_ACTION = 'https://px.ezidebit.com.au/INonPCIService/ChangeCustomerStatus'
     GET_INFO_ACTION = 'https://px.ezidebit.com.au/INonPCIService/GetCustomerDetails'
     EDIT_BANK_ACCOUNT_ACTION = 'https://px.ezidebit.com.au/IPCIService/EditCustomerBankAccount'
     CREATE_SCHEDULE_ACTION = 'https://px.ezidebit.com.au/INonPCIService/CreateSchedule'
     GET_SCHEDULED_PAYMENTS_ACTION = 'https://px.ezidebit.com.au/INonPCIService/GetScheduledPayments'
+    ADD_BANK_DEBIT_ACTION = 'https://px.ezidebit.com.au/IPCIService/AddBankDebit'
 
   	#This method will add a new customer.
     def self.add_customer(options={})
-      response = soap_it!(SOAP_ACTION) do |xml|
+      response = soap_it!(ADD_CUSTOMER_ACTION) do |xml|
    	    xml['px'].AddCustomer do
           xml['px'].DigitalKey Ezidebit::api_digital_key
           options.each { |key,value| xml['px'].send(key, value)}
         end
       end
   	  parse_add_customer_response(response)
+    end
+
+    #This method will add a new customer.
+    def self.edit_customer(options={})
+      response = soap_it!(EDIT_CUSTOMER_ACTION) do |xml|
+        xml['px'].EditCustomerDetails do
+          xml['px'].DigitalKey Ezidebit::api_digital_key
+          options.each { |key,value| xml['px'].send(key, value)}
+        end
+      end
+      parse_generic_status_response(response, 'EditCustomerDetailsResponse')
     end
 
     #This method retrieves details about the given Customer.
@@ -77,8 +90,30 @@ module Ezidebit
         end
       end
       parse_get_scheduled_payments(response)
-
     end
+
+    #Add/Edit customer and payment
+    def self.add_bank_debit(options={})
+      response = soap_it!(true, ADD_BANK_DEBIT_ACTION) do |xml|
+          xml['px'].AddBankDebit do
+            xml['px'].DigitalKey Ezidebit::api_digital_key
+            options.each { |key,value| xml['px'].send(key, value)}
+        end
+      end
+      parse_add_bank_debit_response(response)
+    end
+
+    def self.parse_add_bank_debit_response(response)
+      if response then
+        xml    = Nokogiri::XML(response.body)
+        data   = {}
+        data[:CustomerRef] = xml.xpath("//a:CustomerRef", 
+          {a: 'http://schemas.datacontract.org/2004/07/Ezidebit.PaymentExchange.V3_3.DataContracts'}).text
+        return data
+      else
+        false
+      end
+    end    
 
     def self.parse_add_customer_response(response)
       if response then
